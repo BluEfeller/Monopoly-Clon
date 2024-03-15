ef41e9ec00bd9551dbe672c09aac00080b8d4a4b
BluEfeller <Lukasfeller374@gmx.net>
2024-03-15 10:53:46 +0100

    .

---
+++ Monopoly-Clon_clone_0/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Stets Player 1
+    public static float Player_1_Geld = 1000;
+    public static float Player_1_Feld = 0;
+
+
+    // Stets Player 2
+    public static float Player_2_Geld = 1000;
+    public static float Player_2_Feld = 0;
+
+
+    // Stets Player 3
+    public static float Player_3_Geld = 1000;
+    public static float Player_3_Feld = 0;
+
+
+    // Stets Player 4
+    public static float Player_4_Geld = 1000;
+    public static float Player_4_Feld = 0;
+}
